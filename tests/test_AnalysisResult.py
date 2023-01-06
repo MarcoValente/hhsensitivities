54873b714e0c0f7aa8f6fdce1352a1238f8201a4
Marco Valente <marco.valente@cern.ch>
2023-01-06 15:16:06 +0100

    Adding tests

---
+++ tests/test_AnalysisResult.py
@@ -0,0 +1,17 @@
+import unittest
+from hhdata.AnalysisResult import Experiment,AnalysisResult
+
+class TestAnalysisResults(unittest.TestCase):
+
+    def test_Experiment(self):
+        self.assertEqual(Experiment.ATLAS.name,'ATLAS') 
+        self.assertEqual(Experiment.CMS.name,'CMS') 
+
+    def test_AnalysisResult(self):
+        res = AnalysisResult('AAA',100,'ATLAS')
+        self.assertEqual(res.lumi,100)
+        self.assertEqual(res.name,'AAA')
+        self.assertEqual(res.experiment,'ATLAS')
+        
+if __name__ == '__main__':
+    unittest.main() #This will run all the tests when you execute python test_Models.py
